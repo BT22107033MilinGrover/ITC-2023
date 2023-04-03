6eeacd2038b482279b448942b99291731bb92f29
BT22107033MilinGrover <129713144+BT22107033MilinGrover@users.noreply.github.com>
2023-04-03 13:36:28 +0530

    Update Q1Avg.py

--- Q1Avg.py
+++ Q1Avg.py
@@ -1,7 +1,8 @@
-num1 = float(input("Enter first number: "))
+#inputting integral or decimal values of the numbers that are to be operated
+num1 = float(input("Enter first number: ")) 
 num2 = float(input("Enter second number: "))
 num3 = float(input("Enter third number: "))
-
+#formula to calculate average of three numbers
 average = (num1 + num2 + num3) / 3
-
+#obtaining the output statement
 print("The average of", num1, ",", num2, "and", num3, "is", average)
