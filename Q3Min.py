954e4cf35004640e2baa468817a217eaef786c4e
BT22107033MilinGrover <129713144+BT22107033MilinGrover@users.noreply.github.com>
2023-04-05 09:37:35 +0530

    Update Q3Min.py

--- Q3Min.py
+++ Q3Min.py
@@ -1,10 +1,14 @@
+#integral value for the number of seconds
 no_of_seconds=int(input("Enter no of Seconds:"))
-
+#using if to make sure number of seconds above 60 are included only after no of minutes
 if(no_of_seconds<60):
     print("no of seconds",no_of_seconds,"seconds")
-
+#else to make sure minutes and seconds are displayed properly for input higher than 60 seconds
 else:
     e=no_of_seconds//60
     f=no_of_seconds%60
+    
+  
 
-    print(e,"minutes", f,"seconds")
+
+   print(e,"minutes", "and", f,"seconds")
