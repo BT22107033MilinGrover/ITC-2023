32ae477d4cd20578b1ce3a0306923233af7e35e0
BT22107033MilinGrover <129713144+BT22107033MilinGrover@users.noreply.github.com>
2023-04-05 11:31:49 +0530

    Update Q5Angle.py

--- Q5Angle.py
+++ Q5Angle.py
@@ -2,7 +2,7 @@ import math
 #print table header
 print("Angle\tSine\tCosine
       
- #calculate and print Sine and Cosine for the given angles     
+ #calculate and print Sine and Cosine for the each angle   
 for angle in range(0, 346, 15):
     radians = math.radians(angle)
     sin = round(math.sin(radians), 4)
